372d9c37a9da2be57db0612ad860639debeccc37
Elton Silva <elton.chrls@gmail.com>
2021-08-29 15:18:59 -0300

    COrreçao do algoritmo

--- app/models/export.rb
+++ app/models/export.rb
@@ -10,7 +10,7 @@ class Export
   def process_file!
     read_file = File.read(self.file.tempfile) 
   
-    file_splited = read_file.to_s.split("Envelope-to")
+    file_splited = read_file.to_s.split("Delivery-date: ")
 
     array = []
     re = /<("[^"]*"|'[^']*'|[^'">])*>/
@@ -20,18 +20,36 @@ class Export
     CSV.generate(headers: false, col_sep: ";") do |csv|
       csv << ['data','nome','cpf','telefone','email','cpf_valido']
       file_splited.each_with_index do |line, index|
-        date  = line.split("Delivery-date:")[1][6..25] rescue 'nao identificada'
-       
-        table_split = line.split("Nome: ")[2].split("\r\n") rescue nil
-    
+        date  = line[6..25] rescue 'nao identificada'
+        
+        if line.split("Content preview:").count > 1
+          table_split = line.split("Nome: ")[2].split("<br>") rescue nil
+        else
+          table_split = line.split("Nome: ")[1].split("<br>") rescue nil
+        end
+        
         next if table_split.nil? || date.nil?
         
-        name  = "#{table_split[0].strip}" rescue 'nao informado'
-        cpf   = "#{table_split[1].gsub!('CPF: ', '').gsub("-","").gsub(" ","").gsub(".","").gsub("i","").strip}" rescue 'nao informado'
-        tel   = "#{table_split[2].gsub!('Celular com DDD: ','').strip}" rescue 'nao informado'
-        email = "#{table_split[3].gsub!('Email: ', '').strip}" rescue 'nao informado'
-    
-        @row = "#{name}&#{cpf}&#{tel}&#{email}\n" rescue nil
+        if table_split[0].split('</p>').count > 1
+          
+          table_split_p = table_split[0].split('</p>')
+          name  = "#{table_split_p[0].strip}" rescue 'nao informado'
+          cpf   = "#{table_split_p[1].gsub('CPF: ', '').gsub("<p>","").gsub("-","").gsub(" ","").gsub(".","").gsub("i","").gsub('</p>','').strip}" rescue 'nao informado'
+          tel   = "#{table_split_p[3].split('<div>')[1].gsub("(","").gsub(")","").gsub('-','').gsub('Celular com DDD: ','').gsub("</div>","").gsub(" ","").strip}" rescue 'nao informado'
+          email = "#{table_split_p[3].split('<div>')[2].gsub('Email: ', '').gsub('<p>', '').strip}" rescue 'nao informado'    
+              
+        else
+          if table_split[0].length > 100
+            table_split = table_split[0].split("\r\n")
+          end
+
+          name  = "#{table_split[0].strip}" rescue 'nao informado'
+          cpf   = "#{table_split[1].gsub('CPF: ', '').gsub("<p>","").gsub("-","").gsub(" ","").gsub(".","").gsub("i","").strip}" rescue 'nao informado'
+          tel   = "#{table_split[2].to_s.gsub('Celular com DDD: ','').gsub("(","").gsub(")","").gsub('-','').gsub("<p>","").gsub(" ","").strip}" rescue 'nao informado'
+          email = "#{table_split[3].to_s.gsub('E-mail: ', '').gsub('Email: ', '').strip}" rescue 'nao informado'
+        end
+
+        @row = "#{name.to_s.mb_chars.upcase}&#{cpf}&#{tel}&#{email}\n" rescue nil
 
         csv << [date, name, cpf, tel, email, (CPF.valid?(cpf) ? "CPF VALIDO" : "CPF INVALIDO")]
       end
